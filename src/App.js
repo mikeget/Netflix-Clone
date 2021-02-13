59f34f5e23fee34ea4d064e6e42f7dcbf87ddc3c
Michael Getachew <mikegeta79@gmail.com>
2021-02-12 21:24:00 -0500

    netflic clone

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import "./App.css";
+import requests from "./requests";
+import Row from "./Row";
+import Banner from "./Banner";
+import Nav from './Nav'
+
+
+function App() {
+  return (
+
+    <div className="App">
+      <Nav/>
+      <Banner/>
+      <Row title="NETFLIX ORGINALS"
+        isLargeRow
+        fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Treanding Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRatedMovies}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horrer Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+    </div>
+  );
+}
+
+export default App;
